dadd2f29c2d4ff44a382dad67df6ea561bb395f8
Kallol Khatua <kallolkhatua2005@gmail.com>
2025-02-08 00:55:35 +0530

    update response

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.get("/", async (req, res) => {
-    const response = { statsCode: 200, message: "connected", success: true, port: port };
+    const response = {
+        statsCode: 200,
+        message: "connected",
+        success: true,
+        port: port,
+        env: process.env.DATA,
+        method: req.method,
+        path: req.path
+    };
     console.log(req.method + " " + req.path)
     console.log("Response: ", response)
     res.status(200).send(response)
 })
 
 app.get("/hello-world", async (req, res) => {
-    const response = { statsCode: 200, message: "Hello world", success: true, port: port };
+    const response = {
+        statsCode: 200,
+        message: "Hello world",
+        success: true,
+        port: port,
+        env: process.env.DATA,
+        method: req.method,
+        path: req.path
+    };
     console.log(req.method + " " + req.path)
     console.log("Response: ", response)
     res.status(200).send(response)
